978f28c6c20967f7e190098d15e94fb1f74e24ee
Tara Dehdari <tara.dehdari@gmail.com>
2020-09-14 17:51:40 -0700

    added questions

--- Develop/index.js
+++ Develop/index.js
@@ -2,8 +2,59 @@ const questions = [
     {
         type: 'input',
         name: 'title',
-        message: 'What is the title of your Read Me?'
+        message: 'What is the title of your Project?'
     },
+    {
+        type: 'input',
+        name: 'Description',
+        message: 'Describe your project',
+    }
+    {
+        type: 'input',
+        name: 'Installation',
+        message: 'What are the steps required for Installation of your project',
+    }
+    {
+        type: 'input',
+        name: 'Usage',
+        message: 'What are the instructions for your project',
+    }
+    {
+        type: 'list',
+        name: 'license',
+        choices:[
+            "MIT",
+            "GVL-GPL 3.0",
+            "APACHE 2.0",
+            "BSD 3",
+            "None",
+        ]
+    }
+    {
+        type:'input',
+        name: 'contributors',
+        message:'Who helped contribute?',
+    }
+    {
+        type: 'input',
+        name: 'test',
+        message: 'How do you test your project?',
+    }
+    {
+        type: 'input',
+        name: 'Questions',
+        message: 'Contact information for any further questions',
+    }
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your Github username',
+    }
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your github email?'
+    }
     
 
 ];
