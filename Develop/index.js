6fd7c347ed0350b41617f4098a9261f6bd4b39b2
Tara Dehdari <tara.dehdari@gmail.com>
2020-09-14 17:22:42 -0700

    first commit

---
+++ Develop/index.js
@@ -0,0 +1,18 @@
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your Read Me?'
+    },
+    
+
+];
+
+function writeToFile(fileName, data) {
+}
+
+function init() {
+
+}
+
+init();
