53a3c408397a16d0f5e98aac89624bc2a3732391
Tara Dehdari <tara.dehdari@gmail.com>
2020-09-16 21:48:31 -0700

    edits

--- Develop/index.js
+++ Develop/index.js
@@ -2,9 +2,8 @@ const inquirer = require('inquirer');
 const fs = require ('fs');
 const axios = require('axios');
 const generatorMarkdown = require("./utils/generateMarkdown");
-const api = require('./utils/api');
 
-inquirer.prompt([
+const questions = [
     {
         type: 'input',
         name: 'username',
@@ -22,22 +21,23 @@ inquirer.prompt([
     },
     {
         type: 'input',
-        name: 'Description',
+        name: 'description',
         message: 'Describe your project',
     },
     {
         type: 'input',
         name: 'installation',
-        message: 'What are the steps required for Installation of your project',
+        message: 'What are dependencies are required for Installation of your project',
     },
     {
         type: 'input',
         name: 'usage',
-        message: 'What are the instructions for your project',
+        message: 'What can your app be used for',
     },
     {
         type: 'list',
         name: 'license',
+        message: 'What kind of license does your project have?',
         choices:[
             "MIT",
             "GVL-GPL 3.0",
@@ -55,15 +55,15 @@ inquirer.prompt([
         type: 'input',
         name: 'test',
         message: 'How do you test your project?',
-    },
-    {
-        type: 'input',
-        name: 'questions',
-        message: 'Contact information for any further questions',
     }
+    // {
+    //     type: 'input',
+    //     name: 'questions',
+    //     message: 'Contact information for any further questions',
+    // }
 
 
-]);
+];
 // .then(answers => {
 //     fs.writeFile(`${answers.title}.json`, JSON.stringify(answers, null, 2), (err) => {
 //         if (err) {
@@ -74,22 +74,17 @@ inquirer.prompt([
 //     });
 // });
 
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, function (err){
-        if (err) {
-            throw new Error(err);
-        }
-        console.log('created' + fileName);
-    })
-}
-
 function init() {
     inquirer.prompt(questions)
+    .then(function(data){
+        axios.get(`https://api.github.com/users/${data.UserName}`)
     .then(function (gitData) {
         writeToFile("README.md", generatorMarkdown(data,gitData));
         console.log(gitData);
         console.log(data);
     })
+
+  })
 
 }
 
